20109f2dd1ecb882e72b45542c73e56898b40529
FernandoCrespi <flcrespi@outlook.com>
2026-04-01 12:02:40 -0300

    Tentativa2

--- GerenciadorDeSupermercado/src/model/Usuario.java
+++ GerenciadorDeSupermercado/src/model/Usuario.java
@@ -2,9 +2,33 @@ package model;
 
 public class Usuario {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String nome, cpf;
+	private boolean adm;
+	public Usuario(String nome, String cpf, boolean adm) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.adm = adm;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public boolean isAdm() {
+		return adm;
+	}
+	public void setAdm(boolean adm) {
+		this.adm = adm;
+	}
+	
+	
 
 }
